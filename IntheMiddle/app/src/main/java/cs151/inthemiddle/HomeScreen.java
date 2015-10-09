867ee6857f5fd752d1a255380a027de0fecc98f2
nopenoway0 <kite670@yahoo.com>
2015-10-09 01:31:05 -0700

    Added the homescreen with scrollbox and border

--- IntheMiddle/app/src/main/java/cs151/inthemiddle/HomeScreen.java
+++ IntheMiddle/app/src/main/java/cs151/inthemiddle/HomeScreen.java
@@ -1,32 +1,23 @@
 package cs151.inthemiddle;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
-public class Login extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_home_screen);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
+        getMenuInflater().inflate(R.menu.menu_home_screen, menu);
         return true;
-    }
-
-    public void login(View view){
-        EditText change = (EditText)this.findViewById(R.id.login_credentials);
-        Intent intent = new Intent(this, HomeScreen.class);
-        startActivity(intent);
     }
 
     @Override
