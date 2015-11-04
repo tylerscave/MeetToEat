16e6f17ce33988b8e2164202052af4e9fda23d4e
tylerscave <tylerjones365@gmail.com>
2015-11-04 12:56:08 -0800

    new skeleton with fragments and login/logout

---
+++ app/src/main/java/com/example/lord_tyler/meettoeat/GroupFragment.java
@@ -0,0 +1,12 @@
+package com.example.lord_tyler.meettoeat;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by lord_tyler on 11/3/15.
+ */
+public class GroupFragment extends Fragment {
+
+
+
+}
