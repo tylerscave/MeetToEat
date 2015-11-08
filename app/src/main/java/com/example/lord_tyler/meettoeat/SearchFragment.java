4e3d71ecd96b53bc12fd82d8f53218ec4cc975ef
tylerscave <tylerjones365@gmail.com>
2015-11-07 17:41:44 -0800

    testing github stuff

--- app/src/main/java/com/example/lord_tyler/meettoeat/SearchFragment.java
+++ app/src/main/java/com/example/lord_tyler/meettoeat/SearchFragment.java
@@ -6,5 +6,5 @@ import android.support.v4.app.Fragment;
  * Created by lord_tyler on 11/3/15.
  */
 public class SearchFragment extends Fragment {
-    // just testing github stuff trying to figure out branch updates
+
 }
