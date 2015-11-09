c5b5c64789491e19f78582d2c0ced0063233a6c2
Stephen Cheung <scheung1206@gmail.com>
2015-11-09 12:14:46 -0800

    Added Yelp Classes Nov 9 12:14PM

---
+++ app/src/main/java/com/example/lord_tyler/meettoeat/YelpClasses/API_Static_Stuff.java
@@ -0,0 +1,28 @@
+package com.example.lord_tyler.meettoeat.YelpClasses;
+
+/**
+ * Created by stephencheung on 10/7/15.
+ */
+public class API_Static_Stuff {
+    private final String YELP_CONSUMER_KEY ="ShwF7ALLuZZvMhKMF0n_lw";
+    private final String YELP_CONSUMER_SECRET = "gj16A--fs0B1CNN6NRJnZBTU0xA";
+    private final String YELP_TOKEN = "49lI2VzMR_VTzo8rdQyfoGhkkS9lPTbs";
+    private final String YELP_TOKEN_SECRET = "R9vYA3Ez88I4_83q0ja-l7xGEfs";
+
+
+    public String getYelpConsumerKey(){
+        return YELP_CONSUMER_KEY;
+    }
+
+    public String getYelpConsumerSecret(){
+        return YELP_CONSUMER_SECRET;
+    }
+
+    public String getYelpToken(){
+        return YELP_TOKEN;
+    }
+
+    public String getYelpTokenSecret(){
+        return YELP_TOKEN_SECRET;
+    }
+}
---
+++ app/src/main/java/com/example/lord_tyler/meettoeat/YelpClasses/Span.java
@@ -0,0 +1,18 @@
+package com.example.lord_tyler.meettoeat.YelpClasses;
+
+public class Span {
+	private double latitudeDelta;
+	private double longitudeDelta;
+	public double getLatitudeDelta() {
+		return latitudeDelta;
+	}
+	public void setLatitudeDelta(double latitudeDelta) {
+		this.latitudeDelta = latitudeDelta;
+	}
+	public double getLongitudeDelta() {
+		return longitudeDelta;
+	}
+	public void setLongitudeDelta(double longitudeDelta) {
+		this.longitudeDelta = longitudeDelta;
+	}
+}
