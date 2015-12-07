8afc8aff5fbfb33574c835e4f7d4b4d07d79b92b
Shubaan <shubaan_taheri@yahoo.com>
2015-12-07 13:31:31 -0800

    added Unit Test

---
+++ app/src/androidTest/java/com/example/lord_tyler/meettoeat/SearchTest.java
@@ -0,0 +1,24 @@
+package com.example.lord_tyler.meettoeat;
+
+import android.test.InstrumentationTestCase;
+
+import com.example.lord_tyler.meettoeat.YelpClasses.RunMe;
+
+/**
+ * Created by stephencheung on 12/2/15.
+ */
+public class SearchTest extends InstrumentationTestCase {
+
+    public void test() throws Exception {
+
+        RunMe run = new RunMe();
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                String searching = new RunMe().start("Restaurant", 36.666, -121.444);
+                assertEquals("String", searching);
+            }
+        };
+        myThread.start();
+    }
+}
